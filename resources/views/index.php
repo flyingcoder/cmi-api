a1d72184539f06e3ab47aa088e4d9a8f3d82c87e
Alvin Pacot <aepacot2016@gmail.com>
2016-04-15 08:56:14 +0800

    edit route and change index.php

--- resources/views/index.php
+++ resources/views/index.php
@@ -1,56 +1,86 @@
 <!DOCTYPE html>
-<html ng-app="MyApp">
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>BRIS-GIS</title>
-  <link href="favicon.ico" rel="shortcut icon">
-  <link href="//fonts.googleapis.com/css?family=Roboto|Montserrat:400,700|Open+Sans:400,300,600" rel="stylesheet">
-  <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
-  <link href="//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" rel="stylesheet">
-  <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet">
-  <link href="//cdn.jsdelivr.net/animatecss/3.2.0/animate.css" rel="stylesheet">
-  <link href="app/stylesheets/angular-toastr.css" rel="stylesheet">
-  <link href="app/stylesheets/styles.css" rel="stylesheet">
-</head>
-<body>
-<div ng-controller="NavbarCtrl" ng-cloak class="navbar navbar-default navbar-static-top">
-  <div class="navbar-header">
-    <a class="navbar-brand" href="/#/"><i class="ion-ios7-pulse-strong"></i> <b>BRIS</b>-GIS</a>
-  </div>
-  <ul class="nav navbar-nav">
-    <li ng-if="isAuthenticated()"><a href="/#/profile">Profile</a></li>
-  </ul>
-  <ul ng-if="!isAuthenticated()" class="nav navbar-nav pull-right">
-    <li><a href="/#/login">Login</a></li>
-  </ul>
-  <ul ng-if="isAuthenticated()" class="nav navbar-nav pull-right">
-    <li><a href="/#/logout">Logout</a></li>
-  </ul>
-</div>
+<html ng-app="app" ng-cloak>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>BRIS - GIS</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
-<div ui-view></div>
+    <link rel="stylesheet" type="text/css" href="app/app.css">
+    <!-- Bootstrap 3.3.5 -->
+    <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- jvectormap -->
+    <link rel="stylesheet" href="bower_components/AdminLTE/plugins/jvectormap/jquery-jvectormap-1.2.2.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="bower_components/AdminLTE/dist/css/AdminLTE.min.css">
+    <!-- AdminLTE Skins. Choose a skin from the css/skins
+         folder instead of downloading all of them to reduce the load. -->
+    <link rel="stylesheet" href="bower_components/AdminLTE/dist/css/skins/_all-skins.min.css">
 
-<!-- Third-party Libraries -->
-<script src="bower_components/angular/angular.js"></script>
-<script src="bower_components/angular-animate/angular-animate.js"></script>
-<script src="bower_components/angular-messages/angular-messages.js"></script>
-<script src="bower_components/angular-resource/angular-resource.js"></script>
-<script src="bower_components/angular-sanitize/angular-sanitize.js"></script>
-<script src="bower_components/angular-ui-router/release/angular-ui-router.js"></script>
-<script src="bower_components/angular-toastr/dist/angular-toastr.tpls.js"></script>
-<script src="bower_components/satellizer/satellizer.js"></script>
+    <link href="bower_components/angular-toastr/dist/angular-toastr.css" rel="stylesheet">
 
-<!-- Application Code -->
-<script src="app/app.js"></script>
-<script src="app/directives/passwordStrength.js"></script>
-<script src="app/directives/passwordMatch.js"></script>
-<script src="app/controllers/home.js"></script>
-<script src="app/controllers/login.js"></script>
-<script src="app/controllers/signup.js"></script>
-<script src="app/controllers/logout.js"></script>
-<script src="app/controllers/profile.js"></script>
-<script src="app/controllers/navbar.js"></script>
-<script src="app/services/account.js"></script>
-</body>
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body ng-controller="navController" class="hold-transition" ng-class="{'skin-blue side-mini':isAuthenticated(), 'login-page':!isAuthenticated()}">
+    <div class="wrapper" ui-view>
+        
+    </div>
+
+    <!-- Third-party Libraries -->
+    <script src="bower_components/angular/angular.js"></script>
+    <script src="bower_components/angular-animate/angular-animate.js"></script>
+    <script src="bower_components/angular-messages/angular-messages.js"></script>
+    <script src="bower_components/angular-resource/angular-resource.js"></script>
+    <script src="bower_components/angular-sanitize/angular-sanitize.js"></script>
+    <script src="bower_components/angular-ui-router/release/angular-ui-router.js"></script>
+    <script src="bower_components/angular-toastr/dist/angular-toastr.tpls.js"></script>
+    <!-- Others -->
+    <script src="bower_components/satellizer/satellizer.js"></script>
+    <script src="bower_components/jquery/dist/jquery.js"></script>
+    <script src="bower_components/bootstrap/dist/js/bootstrap.js"></script>
+
+    <!-- Application Code -->
+    <script src="app/app.js"></script>
+    <!--controllers-->
+    <script src="app/modules/core/core.js"></script>
+    <script src="app/modules/dashboard/dashboard.js"></script>
+    <script src="app/modules/user/auth/auth.js"></script>
+    <script src="app/modules/navigation/nav.js"></script>
+    <script src="app/directives/directive.js"></script>
+    <!--script src="app/directives/passwordStrength.js"></script>
+    <script src="app/directives/passwordMatch.js"></script>
+    <script src="app/controllers/home.js"></script>
+    <script src="app/controllers/login.js"></script>
+    <script src="app/controllers/signup.js"></script>
+    <script src="app/controllers/logout.js"></script>
+    <script src="app/controllers/profile.js"></script>
+    <script src="app/controllers/navbar.js"></script>
+    <script src="app/services/account.js"></script-->
+
+    <!-- FastClick -->
+    <script src="bower_components/AdminLTE/plugins/fastclick/fastclick.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="bower_components/AdminLTE/dist/js/app.min.js"></script>
+    <!-- Sparkline -->
+    <script src="bower_components/AdminLTE/plugins/sparkline/jquery.sparkline.min.js"></script>
+    <!-- jvectormap -->
+    <script src="bower_components/AdminLTE/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
+
+    <script src="bower_components/AdminLTE/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+    <!-- SlimScroll 1.3.0 -->
+    <script src="bower_components/AdminLTE/plugins/slimScroll/jquery.slimscroll.min.js"></script>
+    <!-- ChartJS 1.0.1 -->
+    <script src="bower_components/AdminLTE/plugins/chartjs/Chart.min.js"></script>
+
+  </body>
 </html>
